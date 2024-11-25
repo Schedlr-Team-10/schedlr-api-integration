9ddff9ef84e638c9b2bff8ae50165b638d911c62
Ravi Shankar <Bhangeravishankar2302@gmail.com>
2024-11-25 03:14:43 -0500

    Added logger for debugging

--- src/main/java/com/api/schedlr/schedlr_api_integration/controllers/MyProfileController.java
+++ src/main/java/com/api/schedlr/schedlr_api_integration/controllers/MyProfileController.java
@@ -3,11 +3,13 @@ package com.api.schedlr.schedlr_api_integration.controllers;
 import com.api.schedlr.schedlr_api_integration.DTOs.ChangePasswordRequest;
 import com.api.schedlr.schedlr_api_integration.Service.MyProfileService;
 import com.api.schedlr.schedlr_api_integration.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("/myProfile")
 public class MyProfileController {
@@ -26,6 +28,7 @@ public class MyProfileController {
     public ResponseEntity<User> getUserInfo(@RequestParam String userId) {
         try {
             User user = myProfileService.getUserById(Integer.parseInt(userId));
+            log.info("User"+ user);
             return ResponseEntity.ok(user);
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
