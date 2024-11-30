2530f1466fc4c3553fe65f3e9f644c2217a52ad4
varun0080 <kajjamvarunkrishna85@gmail.com>
2024-11-30 14:04:33 -0500

    Eliminated redundant and unused code

--- src/main/java/com/api/schedlr/schedlr_api_integration/Service/InstagramService.java
+++ src/main/java/com/api/schedlr/schedlr_api_integration/Service/InstagramService.java
@@ -3,18 +3,8 @@ package com.api.schedlr.schedlr_api_integration.Service;
 import com.api.schedlr.schedlr_api_integration.Constants.APIConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.http.MediaType;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class InstagramService {
@@ -25,12 +15,12 @@ public class InstagramService {
     private static final String CLIENT_ID = APIConstants.CLIENT_ID;
     private static final String CLIENT_SECRET = APIConstants.CLIENT_SECRET;
 
-    private final String redirectUri = "http://localhost:3000/";  // Replace with your redirect URI
-    private final String state = "DCEeFWf45A53sdfKef424";  // Replace with your state
-    private final String scope = "r_liteprofile%20w_member_social";  // Replace with your scope
+    private final String redirectUri = "http://localhost:3000/";
+    private final String state = "DCEeFWf45A53sdfKef424";
+    private final String scope = "r_liteprofile%20w_member_social";
 
     public String callLinkedInAuthorization() {
-        // Construct the authorization URL
+
         String url = UriComponentsBuilder.fromHttpUrl("https://www.linkedin.com/oauth/v2/authorization")
                 .queryParam("response_type", "code")
                 .queryParam("client_id", CLIENT_ID)
@@ -40,7 +30,7 @@ public class InstagramService {
                 .build()
                 .toUriString();
 
-        // Create a RestTemplate instance
+        
         RestTemplate restTemplate = new RestTemplate();
         String response= response = restTemplate.getForObject(url, String.class);
         System.out.println("Response: " + response);
@@ -49,27 +39,4 @@ public class InstagramService {
         return response;
     }
 
-//    public String uploadImage(MultipartFile fileName, String fileType) {
-//        long fileLength = fileName.getSize();
-//        String appId = APIConstants.appId;
-//        String accessToken = APIConstants.token;
-//        // URL for the API request
-//        String url = "https://graph.facebook.com/v20.0/" + appId + "/uploads"
-//                + "?file_name=" + fileName
-//                + "&file_length=" + fileLength
-//                + "&file_type=" + fileType
-//                + "&access_token=" + accessToken;
-//
-//        // Set headers for the request
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//
-//        // Create request entity
-//        HttpEntity<String> requestEntity = new HttpEntity<>(null, headers);
-//
-//        // Send POST request
-//        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, requestEntity, String.class);
-//
-//        return response.getBody(); // Returns response from Instagram API
-//    }
 }
